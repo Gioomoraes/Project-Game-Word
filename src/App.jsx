5fe658e88ba50a6d1be1034d07a3ac32c181f1dc
Giovanna de Moraes <gigimoraessol@gmail.com>
2025-03-22 18:57:22 -0300

    adicionando check ended conditional

---
+++ src/App.jsx
@@ -0,0 +1,137 @@
+// CSS
+import "./App.css";
+// COMPONENTES
+import StartScreen from "./Components/StartScreen";
+import Game from "./Components/Game";
+import GameOver from "./Components/GameOver";
+// REACT
+import { useCallback, useEffect, useState } from "react";
+// DADOS
+import { wordsList } from "./data/words";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
+
+function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(3);
+  const [score, setScore] = useState(0);
+
+  const pickWordAndCategory = () => {
+    // pick a random category
+    const categories = Object.keys(words);
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    console.log(category);
+
+    // picka rndom words
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
+
+    console.log(word);
+
+    return { word, category };
+  };
+
+  // starts
+  const startGame = () => {
+    // pick words and category
+    const { word, category } = pickWordAndCategory();
+
+    // create a array of letters
+    let wordLetters = word.split("");
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    console.log(word, category);
+    console.log(wordLetters);
+
+    // Fill states
+
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
+
+    setGameStage(stages[1].name);
+  };
+
+  // process the letter input
+  const verifyLetter = (letter) => {
+    const normalizedLetter = letter.toLowerCase();
+    if (
+      guessedLetters.includes(normalizedLetter) ||
+      wrongLetters.includes(normalizedLetter)
+    ) {
+      return;
+    }
+    if (letters.includes(normalizedLetter)) {
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters,
+        normalizedLetter,
+      ]);
+    } else {
+      setWrongLetters((actualWrongLetters) => [
+        ...actualWrongLetters,
+        normalizedLetter,
+      ]);
+
+      setGuesses((actualGuesses) => actualGuesses - 1);
+    }
+  };
+  const clearLetterStates = () => {
+    setGuessedLetters([]);
+    setWrongLetters([]);
+  };
+
+  // Check ended
+  useEffect(() => {
+    if (guesses <= 0) {
+      clearLetterStates();
+
+      setGameStage(stages[2].name);
+    }
+  }, [guesses]);
+
+  // Check win
+  useEffect(() => {
+    const uniqueLetters = [...new set(letters)];
+  }, [guessedLetters]);
+  // restart
+  const retry = () => {
+    setScore(0);
+    setGuesses(3);
+    setGameStage(stages[0].name);
+  };
+
+  return (
+    <div className="App">
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && (
+        <Game
+          verifyLetter={verifyLetter}
+          pickedWord={pickedWord}
+          pickedCategory={pickedCategory}
+          letters={letters}
+          guessedLetters={guessedLetters}
+          wrongLetters={wrongLetters}
+          guesses={guesses}
+          score={score}
+        />
+      )}
+      {gameStage === "end" && <GameOver retry={retry} score={score} />}
+    </div>
+  );
+}
+export default App;
---
+++ src/data/words.jsx
@@ -0,0 +1,8 @@
+export const wordsList = {
+  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+  fruta: ["Maçã", "Banana", "Uva", "Laranja", "Manga"],
+  corpo: ["Cabeça", "Braço", "Perna", "Olho", "Mão"],
+  computador: ["CPU", "Teclado", "Mouse", "Monitor", "Memória RAM"],
+  programação: ["Variável", "Função", "Loop", "Classe", "Objeto"],
+  alimento: ["Arroz", "Feijão", "Carne", "Pão", "Leite"],
+};
